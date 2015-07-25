ba9e68dffd8048c324cf0682c397c1ecef4bebfd
Pooya Sanooei <pooya.source@gmail.com>
2015-07-25 10:43:07 +0430

    Remove reactive and some Sessions

--- client/functionsHelpers.js
+++ client/functionsHelpers.js
@@ -1,53 +1,59 @@
 helperFunctions = {
+  popArray: [],
+  colorArray: [],
+  dimensions: 8,
+  chroms: 56,
+  matrixMain: [],
+  chromNum: 56,
+  popArray3: [],
 
   // Creating the first population
-  firstPop: function(colorNum, chromNum) {
+  firstPop: function () {
+    var dimen = this.dimensions;
+    // Number of colors default is number of nodes
+    var colorNum = $('#colorNum').val() || dimen;
+    // Number of chromosomes default is number of nodes*7
+    var chromNum = $('#chromNum').val() || dimen * 7;
     // Assign each color number to a real color and save it to sessions
-    var cCounter = 0;
-    var colorArray = [];
-    while (cCounter < colorNum) {
-      colorArray[cCounter] = {
-        id: cCounter,
-        color: randomColor()
-      };
-      cCounter++;
-    }
-    Session.set('colorArray', colorArray);
+    var colorArray = this.colorProcess(colorNum);
 
-    // Process the population
-    this.firstPopMake(colorNum, chromNum);
-
-  },
-
-  // Make a random chromosomes list, first population
-  firstPopMake: function(colorNum, chromNum) {
+    // Generate random normal array of chromosomes
     colorNum--;
-    var matrix = Session.get('matrixMain');
-    var dimen = Session.get('dimensions');
+    var matrix = this.matrixMain;
     var allChromes = [];
     var i, j;
-    for(i=0; i<chromNum; i++ ) {
+    for (i = 0; i < chromNum; i++) {
       allChromes[i] = [];
-      for(j=0; j<dimen; j++){
+      for (j = 0; j < dimen; j++) {
         allChromes[i][j] = _.random(0, colorNum);
       }
     }
-    Session.set('firstPopArray', allChromes);
+
+    // Process only fitness column first
+    var fitnessCol = this.fitnessStack(allChromes, dimen, chromNum);
+    console.log(fitnessCol);
+
+    // Reorder fitness column and chromosomes column both
+    var fitnessSorted = this.fitnessSort(allChromes, fitnessCol, dimen);
+
+    this.popArray = fitnessSorted;
+    console.log(fitnessSorted);
+
+    return this.tableOutput(fitnessSorted.a, fitnessSorted.f, colorArray,
+      dimen, chromNum);
   },
 
   // Process two-dimensional array of chromosomes
   // Sending row by row to actual fitness function
-  fitnessStack: function(thisArray) {
-    var chrom = Session.get('chroms');
-    var dimen = Session.get('dimensions');
+  fitnessStack: function (thisArray, dimen, chrom) {
     var smallArray = [];
     var i, j;
     var d;
     var resultArray = [];
-    for(i = 0; i < chrom; i++) {
+    for (i = 0; i < chrom; i++) {
       // Empty one-dimen array everytime
       smallArray = [];
-      for(j =0; j < dimen; j++){
+      for (j = 0; j < dimen; j++) {
         smallArray[j] = thisArray[i][j];
       }
       d = this.fitness(smallArray);
@@ -57,15 +63,16 @@ helperFunctions = {
   },
 
   // Fitness one-dimensional array only, actual fitness rating
-  fitness: function(smallArray) {
+  fitness: function (smallArray) {
+    //console.log(smallArray);
     var bad = 0;
-    var matrix = Session.get('matrixMain');
+    var matrix = this.matrixMain;
     var n = smallArray.length;
-    var i,j;
+    var i, j;
 
-    for(i = 0; i < n; i++) {
-      for(j = 0; j < n; j++) {
-        if(smallArray[i] == smallArray[j] && matrix[i][j] == 1) {
+    for (i = 0; i < n; i++) {
+      for (j = 0; j < n; j++) {
+        if (smallArray[i] == smallArray[j] && matrix[i][j] == 1) {
           bad++; // Less bad is better in our pointing system
           // Using bad so it works with our genetic system
         }
@@ -74,10 +81,121 @@ helperFunctions = {
     return bad;
   },
 
+  // Fitness reorder based on result. smaller numbers on top
+  // all: allchromes 2-dim
+  // f: fitness column 1-dim
+  fitnessSort: function (all, f, dimen) {
+    var fPop = f.slice(); // Not Sorted, another copy we're going to ruin
+
+    var fClone = f.slice(); // Sorted! now let's link it to chromes!
+    fClone.sort(function (a, b) {
+      return a - b;
+    });
+
+    var length = f.length; // Also same as chromNum
+    var newAll = []; // New allchromes 2-dem sorted
+    var temp = []; // Stores f array sorted index numbers
+    var sortedIndex;
+    var i, j, k, l;
+
+    for (i = 0; i < length; i++) {
+      for (j = 0; j < length; j++) {
+        if (fClone[i] == fPop[j]) {
+          temp.push(j);
+          fPop[j] = -1;
+          break;
+        }
+      }
+    }
+    for (k = 0; k < length; k++) {
+      sortedIndex = temp[k];
+      newAll[k] = [];
+      for (l = 0; l < dimen; l++) {
+        newAll[k][l] = all[sortedIndex][l];
+      }
+    }
+
+    var result = {
+      f: fClone,
+      a: newAll
+    };
+    return result;
+  },
+
+  // m2: all chromes 2-dimension array
+  // fitnessCol: fitness col
+  // c: color array
+  // h: dimen
+  // chrom: number of chroms
+  // dontColor: don't color these last indexes(or rows)
+  tableOutput: function (m2, fitnessColf, c, h, chrom, dontColor) {
+    var dontColor = dontColor || 0;
+    var headers = '';
+    var matrixOut = '';
+    var thisObject = {};
+    var thisCounter;
+
+    // Which indexes won't get color styles
+    var u;
+    var indexOfDontColor = 0;
+    for (u = 0; u < dontColor; u++) {
+      // Value will start from last indexes
+      indexOfDontColor = m2.length - u - 1;
+    }
+
+    if (m2 != '') {
+      // Printing headers
+      for (var i = 0; i < h; i++) {
+        var headers = headers +
+          '<th class="tooltipped" data-position="top" ' +
+          'data-tooltip="شماره نود">'
+          + i + '</th>';
+      }
+      headers = headers + '<th class="fitness-col">شایستگی<th>';
+      // Printing rows
+      for (var j = 0; j < chrom; j++) {
+        var matrixOut = matrixOut + '<tr class="tooltipped"' +
+          ' data-position="bottom" data-tooltip="شماره ردیف یا کروموزوم: '
+          + j + '" >';
+
+        // Printing rows
+        for (var k = 0; k < h; k++) {
+          var thisCounter = m2[j][k];
+          var thisObject = c[thisCounter];
+          if (thisObject != undefined) { // If this chrom exist in colors
+            var matrixOut = matrixOut + '<td ' +
+              'style="background-color: ' +
+              thisObject.color + ' ">' + thisCounter + '</td>';
+          } else { // If it doesn't it's a new row after tarkib
+            var matrixOut = matrixOut + '<td ' +
+              '>' + thisCounter + '</td>';
+          }
+        }
+
+        // Adding fitness answer to last column of each row
+        matrixOut = matrixOut + '<td class="fitness-col">'
+          + fitnessColf[j] + '</td>';
+        var matrixOut = matrixOut + '</tr>';
+      }
+    }
+
+    var tableArray = {
+      headers: headers,
+      matrixOutput: matrixOut
+    };
+
+    Meteor.setTimeout(function () {
+      // Initialize tooltips.
+      $('.tooltipped').tooltip({delay: 50});
+    }, 1000);
+
+    return tableArray;
+  },
+
   // Prepare inputs.
   // First create a matrix array from inputs, then converts it to D3 list
   // and then calls drawGraph method using the list object.
-  prepGraph: function() {
+  prepGraph: function () {
     // Get matrix
     var matrix_row = [];
 
@@ -95,16 +213,19 @@ helperFunctions = {
       }
     });
     // Other functions use this array, we'll make it accessible
-    Session.set('matrixMain', matrix_row);
+    //Session.set('matrixMain', matrix_row);
+    this.matrixMain = matrix_row;
 
     // convert to list
-    var names =[];
+    var names = [];
     var links = [],
       n = matrix_row.length;
 
-    var i = -1; while (++i < n) {
+    var i = -1;
+    while (++i < n) {
       names.push({"name": i});
-      var j = -1; while (++j < n) {
+      var j = -1;
+      while (++j < n) {
         links.push({
           source: i,
           target: j,
@@ -120,9 +241,11 @@ helperFunctions = {
     // Now Actually draw Graph using D3 library
     this.drawGraph(newGraphList);
   },
+
+  // D3 codes
   drawGraph: function (graph) {
-    var width = 450,
-      height = 450;
+    var width = 350,
+      height = 350;
 
     // First remove previous drawings
     d3.select("svg").remove();
@@ -147,48 +270,49 @@ helperFunctions = {
       .data(graph.links)
       .enter().append("line")
       .attr("class", "link")
-      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+      .style("stroke-width", function (d) {
+        return Math.sqrt(d.value);
+      });
 
     var node = svg.selectAll(".node")
       .data(graph.nodes)
       .enter().append("circle")
       .attr("class", "node")
       .attr("r", 5)
-      .style("fill", function(d) { return color(d.group); })
+      .style("fill", function (d) {
+        return color(d.group);
+      })
       .call(force.drag);
 
     node.append("title")
-      .text(function(d) { return d.name; });
+      .text(function (d) {
+        return d.name;
+      });
 
-    force.on("tick", function() {
-      link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+    force.on("tick", function () {
+      link.attr("x1", function (d) {
+        return d.source.x;
+      })
+        .attr("y1", function (d) {
+          return d.source.y;
+        })
+        .attr("x2", function (d) {
+          return d.target.x;
+        })
+        .attr("y2", function (d) {
+          return d.target.y;
+        });
 
-      node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
+      node.attr("cx", function (d) {
+        return d.x;
+      })
+        .attr("cy", function (d) {
+          return d.y;
+        });
     });
   },
 
-  matrixToList : function(matrix) {
-    var links = [],
-      n = matrix.length;
-
-    var i = -1; while (++i < n) {
-      var j = -1; while (++j < n) {
-        links.push({
-          source: i,
-          target: j,
-          value: matrix[i][j]
-        });
-      }
-    }
-    console.log(matrix);
-    console.log(links);
-    return links;
-  },
-  drawInput: function(rows) {
+  drawInput: function (rows) {
     var columns = rows;
     var form = document.getElementById("frm");
     for (var i = 0; i < rows; i++) {
@@ -203,7 +327,146 @@ helperFunctions = {
       var br = $('<br>')[0];
       form.appendChild(br);
     }
+  },
+
+  // Assign each color number to a real color
+  // store it in object in a good structire
+  // and save it to sessions
+  colorProcess: function (colorNum) {
+    var cCounter = 0;
+    var colorArray = [];
+    while (cCounter < colorNum) {
+      colorArray[cCounter] = {
+        id: cCounter,
+        color: randomColor()
+      };
+      cCounter++;
+    }
+    //Session.set('colorArray', colorArray);
+    this.colorArray = colorArray;
+    return colorArray;
+  },
+
+  tarkib: function () {
+    var dimen = $('#row').val() || 8;
+    // Number of colors default is number of nodes
+    var colorNum = $('#colorNum').val() || dimen;
+    // Number of chromosomes default is number of nodes*7
+    var chromNum = $('#chromNum').val() || dimen * 7;
+    // Number of chromosomes default is number of nodes*7
+    //var popArray = Session.get('popArray');
+    var popArray = this.popArray;
+    //console.log(popArray);
+
+    // Generate 2 random indexes
+    var chromZero = chromNum - 1;
+    var one = _.random(0, chromZero);
+    var sw = 0;
+    // Making sure they're not same index
+    while (sw == 0) {
+      var two = _.random(0, chromZero);
+      if (two != one)
+        sw = 1;
+    }
+
+    return this.tableOutput3(popArray, dimen, chromNum, one, two);
+  },
+
+  tableOutput3: function (popArray, dimen, chromNum, one, two) {
+    var dimenZero = dimen - 1;
+
+    var all = popArray.a;
+    var fitnessCol = popArray.f;
+
+    var i, r, r2;
+    var firstTable = '';
+    var secondTable = '';
+    var firstSelected = [];
+    var secondSelected = [];
+    var firstSelectedC = [];
+    var secondSelectedC = [];
+    var firstF, secondF;
+    //var secondFixed[];
+
+    // Print 2 random chromosomes
+    firstTable = firstTable + '<tr>';
+    for (i = 0; i < dimen; i++) {
+      // Also create new one-dim array
+      firstSelected[i] = all[one][i];
+      // Print
+      firstTable = firstTable + '<td>' + all[one][i] + '</td>';
+    }
+    firstTable = firstTable + '</tr>';
+    firstTable = firstTable + '<tr>';
+    for (i = 0; i < dimen; i++) {
+      // Also create new one-dim array
+      secondSelected[i] = all[two][i];
+      // Print
+      firstTable = firstTable + '<td>' + all[two][i] + '</td>';
+    }
+    firstTable = firstTable + '</tr>';
+
+    // Change structure for tarkib
+    firstSelectedC = firstSelected.slice();
+    secondSelectedC = secondSelected.slice();
+    var tarkibNum = parseInt(dimen / 2);
+    for (i = 0; i < tarkibNum; i++) {
+      r = _.random(0, dimenZero);
+      r2 = _.random(0, dimenZero);
+      firstSelectedC[r] = secondSelected[r2];
+      secondSelectedC[r] = firstSelected[r2];
+    }
+    // We changed tarkib
+
+    // Print second
+    // Print 2 random chromosomes
+    secondTable = secondTable + '<tr>';
+    for (i = 0; i < dimen; i++) {
+      secondTable = secondTable + '<td>' + firstSelectedC[i] + '</td>';
+    }
+    secondTable = secondTable + '</tr>';
+    secondTable = secondTable + '<tr>';
+    for (i = 0; i < dimen; i++) {
+      secondTable = secondTable + '<td>' + secondSelectedC[i] + '</td>';
+    }
+    secondTable = secondTable + '</tr>';
+
+    // Adding 2 news tarkib rows to end of all chromes table
+    // Also add selected to end of old popArray to print all the table later
+    for (i = 0; i < dimen; i++) {
+      all[chromNum] = [];
+      all[chromNum][i] = firstSelectedC[i];
+      all[chromNum + 1] = [];
+      all[chromNum + 1][i] = secondSelectedC[i];
+    }
+
+    // Fitness for new columns
+    firstF = this.fitness(firstSelectedC);
+    fitnessCol.push(firstF);
+    secondF = this.fitness(secondSelectedC);
+    fitnessCol.push(secondF);
+
+
+    this.chromNum = chromNum;
+
+    // Printing first two columns
+    var tableOutput = {
+      one: one,
+      two: two,
+      first: firstTable,
+      second: secondTable
+    };
+
+    // To print new chromosomes again
+    var result = {
+      f: fitnessCol,
+      a: all
+    };
+    this.popArray3 = result;
+
+    return tableOutput;
   }
 
 };
+
 
--- client/templateHelpers.js
+++ client/templateHelpers.js
@@ -1,73 +1,3 @@
-Template.section2.helpers({
-  colors: function() {
-    return Session.get('colorArray');
-  },
-  dimensions: function() {
-    return Session.get('dimensions');
-  },
-  chroms: function() {
-    return Session.get('chroms');
-  }
-});
-
-Template.section2table.helpers({
-  tableOutput: function() {
-    // Need this for background-color
-    var c = Session.get('colorArray');
-    // Number of columns is same as nodes
-    var h = Session.get('dimensions');
-    // If firstPopArray is undefined yet: first-pop btn haven't happened yet
-    var m = Session.get('firstPopArray') || '';
-    var headers = '';
-    var matrixOut = '';
-
-    // Adding fitness column
-    var fitnessCol = helperFunctions.fitnessStack(m);
-
-    if(m != '') {
-      for (var i = 0; i < h; i++) {
-        var headers = headers +
-          '<th class="tooltipped" data-position="top" ' +
-          'data-tooltip="شماره نود">'
-          + i + '</th>';
-      }
-      headers = headers + '<th class="fitness-col">شایستگی<th>';
-      var dimen = Session.get('dimensions');
-      var chrom = Session.get('chroms');
-      for (var j = 0; j < chrom; j++) {
-        var matrixOut = matrixOut + '<tr class="tooltipped"' +
-          ' data-position="bottom" data-tooltip="شماره ردیف یا کروموزوم: '
-          + j + '" >';
-        for (var k = 0; k < dimen; k++) {
-          thisCounter = m[j][k];
-          var thisObject = c[thisCounter];
-          var matrixOut = matrixOut + '<td ' +
-            'style="background-color: ' +
-            thisObject.color + ' ">' + thisCounter + '</td>';
-        }
-        // Adding fittness answer to last column of each row
-        matrixOut = matrixOut + '<td class="fitness-col">'
-          + fitnessCol[j] + '</td>';
-        var matrixOut = matrixOut + '</tr>';
-      }
-    }
-
-    var tableArray = {
-      headers: headers,
-      matrixOutput: matrixOut
-    };
-
-
-    Meteor.setTimeout(function(){
-      // Initialize tooltips.
-      $('.tooltipped').tooltip({delay: 50});
-    }, 1000);
-
-    return tableArray;
-  }
-});
-
-
 Template.section1.events({
   'click button.gen-graph': function () {
     helperFunctions.prepGraph();
@@ -79,9 +9,9 @@ Template.section1.events({
     $('#frm').html('');
     var rows = $('#row').val() || 8;
     // Number of colors default is number of nodes
-    Session.set('dimensions', rows);
+    helperFunctions.dimensions = rows;
     // Number of chromosomes default is number of nodes*7
-    Session.set('chroms', rows * 7); // dim * 7
+    helperFunctions.chroms = rows * 7; // dim * 7
     helperFunctions.drawInput(rows);
     Materialize.toast('ابعاد آرایه ماتریکس تغییر یافت', 2000);
   },
@@ -93,19 +23,64 @@ Template.section1.events({
   }
 });
 
-Template.section2.events({
-  // First Population
-  'click #first-pop': function () {
-    // Number of colors default is number of nodes
-    var colorNum = $('#colorNum').val() || Session.get('dimensions');
-    // Number of chromosomes default is number of nodes*7
-    var chromNum = $('#chromNum').val() || Session.get('dimensions')*7;
-    helperFunctions.firstPop(colorNum, chromNum);
-    Materialize.toast('جمعیت اولیه تولید شد', 3000);
+Template.section2.helpers({
+  colors: function () {
+    return helperFunctions.colorArray;
   },
-  'change #chromNum': function() {
-    Session.set('chroms', $('#chromNum').val());
+  dimensions: function () {
+    return helperFunctions.dimensions;
+  },
+  chroms: function () {
+    return helperFunctions.chroms;
   }
 });
 
+Template.section2table.helpers({
+  tableOutput: function () {
+    Session.get('section2Update');
+    var output = helperFunctions.firstPop();
+    return output;
+  }
+});
+
+Template.section2.events({
+  // First Population
+  'click #first-pop': function () {
+    Session.set('section2Update', new Date());
+    Materialize.toast('جمعیت اولیه تولید شد', 3000);
+  },
+  'change #chromNum': function () {
+    helperFunctions.chroms = $('#chromNum').val();
+  }
+});
+
+Template.section3.events({
+  // Tarkib
+  'click #tarkib': function () {
+    // Triggers section3table helper which trigers tarkib()
+    Session.set('section3Update', new Date());
+    Materialize.toast('ترکیب انجام شد', 3000);
+  }
+});
+
+Template.section3table.helpers({
+  tableOutput: function () {
+    Session.get('section3Update');
+    var output = helperFunctions.tarkib();
+    return output;
+  },
+  tableOutputAll: function () {
+    //var newTable = Session.get('popArray3');
+    var newTable = helperFunctions.popArray3;
+    //console.log(newTable);
+    //var colorArray = Session.get('colorArray');
+    var colorArray = helperFunctions.colorArray;
+    //var dimen = Session.get('dimensions');
+    var dimen = helperFunctions.dimensions;
+    //var chromNum = Session.get('chromNum');
+    var chromNum = helperFunctions.chromNum;
+    return helperFunctions.tableOutput(newTable.a, newTable.f, colorArray,
+      dimen, chromNum);
+  }
+});
 
